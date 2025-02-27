0518fd0cc7e3c41e65a5d78139e7ad496057a828
Shan Yan <shan.yan@kellogg.northwestern.edu>
2025-02-27 17:51:29 +0000

    practice question auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,27 @@ class SessionsController < ApplicationController
   
   def create
     # TODO: authenticate user
-    flash["notice"] = "Nope."
-    redirect_to "/login"
+    @user = User.find_by({"email" => params["email"]})
+
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome, #{@user["first_name"]}"
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      end 
+    else 
+      flash["notice"] = "Nope."
+      redirect_to "/login"
   end
+end
+
+def destroy
+  flash["notice"] = "Goodbye."
+  session["user_id"] = nil 
+  redirect_to "/login"
+end
+
 end
